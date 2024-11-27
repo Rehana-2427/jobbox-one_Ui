0da4b169297633d162325c98677da25d143b57aa
Rehana2427 <shaikrehana248@gmail.com>
2024-11-27 12:28:30 +0530

    commit change in chat component of long message sholuld be wrap without space

--- src/Dashboards/ChatComponent.jsx
+++ src/Dashboards/ChatComponent.jsx
@@ -9,10 +9,7 @@ import { MdDelete, MdEdit } from "react-icons/md";
 import SockJS from "sockjs-client";
 import Swal from "sweetalert2";
 import './ChatComponent.css';
-
-
 const BASE_API_URL = process.env.REACT_APP_API_URL;
-
 const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOpen }) => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
@@ -20,7 +17,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
   const [connected, setConnected] = useState(false);
   const chatBoxRef = useRef(null);
   const [editMessageId, setEditMessageId] = useState(null); // To track which message is being edited
-
   // Helper function to format time
   function formatMessageDateTime(timestamp) {
     const date = new Date(timestamp);
@@ -28,15 +24,12 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
       console.error("Invalid date:", timestamp);
       return "Invalid Date";
     }
-
     const hours = date.getHours();
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const ampm = hours >= 12 ? 'PM' : 'AM';
     const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-
     return `${formattedHours}:${minutes} ${ampm}`;
   }
-
   // Helper function to format date
   function formatDate(timestamp) {
     const date = new Date(timestamp);
@@ -44,7 +37,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
       console.error("Invalid date:", timestamp);
       return "Invalid Date";
     }
-
     if (isToday(date)) {
       return "Today";
     } else if (isYesterday(date)) {
@@ -54,6 +46,7 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
     }
   }
 
+  
   const [userName, setUserName] = useState('');
   useEffect(() => {
     const fetchUserName = async () => {
@@ -72,12 +65,11 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
               userId: hrId
             }
           });
-          setUserName(
-            response.data.companyName.charAt(0).toUpperCase() +
-            response.data.companyName.slice(1).toLowerCase() +
-            ' HR'
-          );
+          const formatUserName = (name) => {
+            return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+          };
 
+          setUserName(formatUserName(response.data.companyName) + ' HR');
         }
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -87,8 +79,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
     }
     fetchUserName();
   }, [userType, hrId, candidateId]); // Runs when userType changes
-
-
   // Fetching messages on component mount
   useEffect(() => {
     const fetchMessages = async () => {
@@ -97,20 +87,16 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
         const allMessages = response.data;
         const today = new Date();
         const twoDaysAgo = subDays(today, 2);
-
         const filteredMessages = allMessages.filter(msg => {
           const messageDate = new Date(msg.timestamp);
           return isBefore(messageDate, twoDaysAgo) === false;
         });
-
         setMessages(filteredMessages);
       } catch (error) {
         console.error('Error fetching messages:', error);
       }
     };
-
     fetchMessages();
-
     const socket = new SockJS(`${BASE_API_URL}/ws`);
     const client = new Client({
       webSocketFactory: () => socket,
@@ -120,30 +106,22 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
         //   const message = JSON.parse(messageOutput.body);
         //   setMessages((prevMessages) => [...prevMessages, message]);
         // });
-
         client.subscribe(`/topic/app`, (messageOutput) => {
           const message = JSON.parse(messageOutput.body);
-          // setMessages((prevMessages) => {
-
-          //   if (message.chatId) {
-
-          //   }
-          //   // Check if the message is an update
-          //   const existingMessageIndex = prevMessages.findIndex(msg => msg.chatId === message.chatId);
-          //   if (existingMessageIndex > -1) {
-          //     // Update the existing message
-          //     const updatedMessages = [...prevMessages];
-          //     updatedMessages[existingMessageIndex] = message;
-          //     return updatedMessages;
-          //   } else {
-          //     // If it's a new message, just append it
-          //     return [...prevMessages, message];
-          //   }
-          // });
-
+          setMessages((prevMessages) => {
+            // Check if the message is an update
+            const existingMessageIndex = prevMessages.findIndex(msg => msg.chatId === message.chatId);
+            if (existingMessageIndex > -1) {
+              // Update the existing message
+              const updatedMessages = [...prevMessages];
+              updatedMessages[existingMessageIndex] = message;
+              return updatedMessages;
+            } else {
+              // If it's a new message, just append it
+              return [...prevMessages, message];
+            }
+          });
         });
-
-
       },
       onDisconnect: () => {
         console.log('Disconnected from WebSocket');
@@ -153,37 +131,29 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
         console.error('STOMP Error: ', error);
       },
     });
-
     client.activate();
     stompClientRef.current = client;
-
     return () => {
       if (stompClientRef.current) {
         stompClientRef.current.deactivate();
       }
     };
   }, [applicationId]);
-
   // Auto scroll the chat to the bottom when new messages arrive
   useEffect(() => {
     if (chatBoxRef.current) {
       chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
     }
   }, [messages]);
-
   const [isSending, setIsSending] = useState(false);
-
   // Send new message
   const sendMessage = () => {
     if (!newMessage.trim()) return;
-
     const stompClient = stompClientRef.current;
-
     if (!stompClient || !stompClient.connected) {
       console.error("STOMP client is not connected!");
       return;
     }
-
     const message = {
       applicationId,
       hrMessage: hrId && userType === 'HR' ? newMessage : '',
@@ -192,20 +162,16 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
       isCandidateRead: false,
       timestamp: new Date().toISOString(),
     };
-
     stompClient.publish({
       destination: '/app/chat',
       body: JSON.stringify(message),
     });
-
     setNewMessage("");
   };
-
   // Close chat
   const handleClose = () => {
     setIsChatOpen(false);
   };
-
   // Delete message with confirmation
   const handleDelete = (chatId) => {
     Swal.fire({
@@ -223,20 +189,17 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
       }
     });
   };
-
   // Delete message from server
   const deleteMsg = async (chatId) => {
     try {
       await axios.delete(`${BASE_API_URL}/deleteChatMsg`, { params: { chatId: chatId } });
       setMessages(messages.filter((msg) => msg.chatId !== chatId));
-
       Swal.fire("Deleted!", "Your message has been deleted.", "success");
     } catch (error) {
       console.error("Error deleting message:", error);
       Swal.fire("Error!", "Failed to delete the message.", "error");
     }
   };
-
   // Edit message
   const handleUpdate = (chatId) => {
     const messageToEdit = messages.find((msg) => msg.chatId === chatId);
@@ -245,7 +208,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
       setEditMessageId(chatId); // Track the ID of the message being edited
     }
   };
-
   const handleUpdateMessage = async () => {
     setIsSending(true);
     try {
@@ -256,19 +218,16 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
         candidateMessage: candidateId && userType === 'Candidate' ? newMessage : '',
         timestamp: new Date().toISOString(),
       };
-
       // Update the message on the server
       await axios.put(`${BASE_API_URL}/updateChatMsg`, updatedMessage, {
         params: { chatId: editMessageId },
       });
-
       // Update the message in the local state
       setMessages((prevMessages) =>
         prevMessages.map((msg) =>
           msg.chatId === editMessageId ? { ...msg, ...updatedMessage } : msg
         )
       );
-
       // Publish the updated message to the WebSocket topic
       const stompClient = stompClientRef.current;
       if (stompClient && stompClient.connected) {
@@ -277,7 +236,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
           body: JSON.stringify(updatedMessage),
         });
       }
-
       setNewMessage('');
       setEditMessageId(null);
       Swal.fire("Updated!", "Your message has been updated.", "success");
@@ -288,7 +246,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
       setIsSending(false);
     }
   };
-
   return (
     <Modal show={true} onHide={handleClose} style={{ overflowY: 'auto' }}>
       <Modal.Header closeButton>
@@ -298,7 +255,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
         <div className="chat-box" ref={chatBoxRef} style={{ flexGrow: 1, overflowY: 'auto' }}>
           {messages.map((msg, msgIndex) => {
             const messageDate = formatDate(msg.createdAt);
-
             return (
               <div key={msg.chatId}>
                 {msgIndex === 0 || formatDate(messages[msgIndex - 1].createdAt) !== messageDate ? (
@@ -306,7 +262,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
                     <strong>{messageDate}</strong>
                   </div>
                 ) : null}
-
                 <div className={`message-container ${msg.hrMessage
                   ? userType === 'HR' ? 'message-left' : 'message-right'
                   : userType === 'Candidate' ? 'message-left' : 'message-right'
@@ -319,7 +274,6 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
                       <span>{formatMessageDateTime(msg.createdAt)}</span>
                     </div>
                   </div>
-
                   {/* Conditionally render edit and delete icons */}
                   {((userType === 'Candidate' && msg.candidateMessage) || (userType === 'HR' && msg.hrMessage)) && (
                     <>
@@ -358,7 +312,7 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
               onClick={handleUpdateMessage}
               disabled={isSending}
             >
-             <FontAwesomeIcon icon={faPaperPlane} /> {isSending ? 'Updating...' : 'Update Message'}
+              <FontAwesomeIcon icon={faPaperPlane} /> {isSending ? 'Updating...' : 'Update Message'}
             </Button>
           ) : (
             <Button
@@ -367,7 +321,7 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
               onClick={sendMessage}
               disabled={isSending}
             >
-             <FontAwesomeIcon icon={faPaperPlane} /> {isSending ? 'Sending...' : 'Send Message'}
+              <FontAwesomeIcon icon={faPaperPlane} /> {isSending ? 'Sending...' : 'Send Message'}
             </Button>
           )}
         </div>
@@ -375,5 +329,13 @@ const ChatComponent = ({ applicationId, hrId, candidateId, userType, setIsChatOp
     </Modal>
   );
 };
-
 export default ChatComponent;
+
+
+
+
+
+
+
+
+
